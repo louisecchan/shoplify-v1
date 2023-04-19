127bf1250f51a9120d6731b5f7229768a26a5192
Louise <louise.cch@gmail.com>
2023-04-19 22:44:38 +0100

    Add animations on scroll

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -8,9 +8,26 @@ const FeaturedProducts = ({ type }) => {
     `/products?populate=*&[filters][type][$eq]=${type}`
   );
 
+  function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+  window.addEventListener("scroll", reveal);
+
   return (
     <div className="featuredProducts">
-      <div className="top">
+      <div className="top reveal">
         <h1>{type} products</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem sapiente
@@ -19,7 +36,7 @@ const FeaturedProducts = ({ type }) => {
           inventore.
         </p>
       </div>
-      <div className="bottom">
+      <div className="bottom reveal">
         {error
           ? "Something went wrong!"
           : loading

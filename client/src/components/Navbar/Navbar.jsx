43f34e23104834ffff8940663b831b6cc93c8d4f
Louise <louise.cch@gmail.com>
2023-04-25 20:59:41 +0100

    Add hamburger menu

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -14,6 +14,13 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="wrapper">
+        <button className="hamburger">
+          <svg viewBox="0 0 100 80" width="40" height="40">
+            <rect width="100" height="3"></rect>
+            <rect y="30" width="100" height="3"></rect>
+            <rect y="60" width="100" height="3"></rect>
+          </svg>
+        </button>
         <div className="left">
           <div className="item">
             <img src="/img/locale.png" className="localeImg" alt="" />
